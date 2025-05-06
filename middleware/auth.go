754254b4861a180815d4d9a283c28183aad76154
unclesam <2036253946@qq.com>
2025-05-06 13:39:01 +0800

    feat(middleware): 添加认证中间件和登出功能
    - 新增 AuthMiddleware 和 AuthAdminMiddleware 中间件用于用户认证
    - 实现 Logout 和 HashLogout 函数用于处理用户登出
    - 添加 GetAuth 函数用于获取认证信息

---
+++ middleware/auth.go
@@ -0,0 +1,55 @@
+package middleware
+
+import (
+	"FastFiber_v2/service/redis_ser"
+	"FastFiber_v2/utils/jwts"
+	"FastFiber_v2/utils/res"
+	"github.com/gofiber/fiber/v2"
+)
+
+func AuthMiddleware() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		accessToken := c.Get("access_token")
+		_, err := jwts.CheckAccessToken(accessToken)
+		if err != nil {
+			return res.FailWithMsg("认证失败", c)
+		}
+		if redis_ser.HashLogout(accessToken); err != nil {
+			return res.FailWithMsg("当前登录已经注销", c)
+		}
+		return c.Next()
+	}
+}
+
+func AuthAdminMiddleware() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		accessToken := c.Get("access_token")
+		claims, err := jwts.CheckAccessToken(accessToken)
+		if err != nil {
+			return res.FailWithMsg("认证失败", c)
+		}
+		if redis_ser.HashLogout(accessToken) {
+			return res.FailWithMsg("当前登录已经注销", c)
+		}
+		if claims.RoleID != 1 {
+			return res.FailWithMsg("认证失败", c)
+		}
+		c.Locals("claims", claims)
+		return c.Next()
+	}
+}
+
+func GetAuth(c *fiber.Ctx) (cl *jwts.Payload) {
+	cl = new(jwts.Payload)
+	_claims := c.Locals("claims")
+	if _claims == nil {
+		return nil
+	}
+
+	cl, ok := _claims.(*jwts.Payload)
+	if !ok {
+		return nil
+	}
+
+	return cl
+}
